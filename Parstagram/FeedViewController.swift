e403858afa25b816d6dfb370f33ba2fe5e43f78b
Earl Tankard, Jr <theflashtank21@gmail.com>
2021-03-22 00:42:12 -0400

    Parstagram Applicatoin -- Part II

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -8,26 +8,52 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate {
+    let commentBar = MessageInputBar()
+    var showCommentsBar = false
     var posts = [PFObject]()
+    var selectedPost: PFObject!
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        commentBar.inputTextView.placeholder = "Add a comment..."
+        commentBar.sendButton.title = "Post"
+        commentBar.delegate  = self
         tableView.delegate   = self
         tableView.dataSource = self
+        tableView.keyboardDismissMode = .interactive
+        
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
 
         // Do any additional setup after loading the view.
     }
+    
+    @objc func keyboardWillBeHidden(note: Notification) {
+        commentBar.inputTextView.text = nil
+        showCommentsBar = false
+        becomeFirstResponder()
+    }
+    
+    override var inputAccessoryView: UIView? {
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return showCommentsBar
+    }
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground {(posts, error) in
@@ -40,26 +66,107 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        
+        //create comment
+        let comment = PFObject(className: "Comments")
+        comment["text"]   = text
+        comment["post"]   = selectedPost
+        comment["author"] = PFUser.current()!
+
+        selectedPost.add(comment, forKey: "comments")
+        selectedPost.saveInBackground {(success, error) in
+            if success {
+                print("comment saved")
+            }
+            else {
+                print("Error saving comm")
+            }
+        }
+        
+        tableView.reloadData()
+        
+        //clear and dismiss bar
+        commentBar.inputTextView.text = nil
+        showCommentsBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post     = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post      = posts[indexPath.section]
+        let comments  = (post["comments"] as? [PFObject]) ?? []
         
-        let post      = posts[indexPath.row]
-        let user      = post["author"] as! PFUser
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url       = URL(string: urlString)!
+        if indexPath.row == 0 {
+            let cell      = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            let user      = post["author"] as! PFUser
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url       = URL(string: urlString)!
+            
+            
+            cell.usernameLabel.text = user.username
+            cell.captionLabel.text  = (post["caption"] as! String)
+            cell.photoView.af.setImage(withURL: url)
+            
+            return cell
+        }
         
+        else if indexPath.row <= comments.count {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCommentCell")!
+            return cell
+        }
         
-        cell.usernameLabel.text = user.username
-        cell.captionLabel.text  = (post["caption"] as! String)
-        cell.photoView.af.setImage(withURL: url)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post    = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
+        if indexPath.row == comments.count + 1 {
+            showCommentsBar = true
+            becomeFirstResponder()
+            commentBar.inputTextView.becomeFirstResponder()
+            
+            selectedPost = post
+        }
+    }
+    
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
         
-        return cell
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        // credit for getting this to work comes from: https://gist.github.com/alexpaul/875d1c8ce45a5f536d0c81087285f4d8
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let sceneDelegate = windowScene.delegate as? SceneDelegate
+          else {
+            return
+          }
+
+        sceneDelegate.window?.rootViewController = loginViewController
     }
     
 
@@ -72,5 +179,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
 }
